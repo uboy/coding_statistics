7b8fe476ad558b657f2d9936b26e2b33c4dd00d9
Denis Mazur <mazur.denis@huawei.com>
2025-07-04 17:57:34 +0300

    added commit stats for unified review for all sites

--- unified_review_stat.py
+++ unified_review_stat.py
@@ -21,12 +21,10 @@ HEADERS = [
     "Additions", "Deletions", "Reviewers"
 ]
 
-
 def load_config():
     config = ConfigParser()
     config.read(CONFIG_FILE, encoding="utf-8-sig")
     return config
-
 
 def init_session(token=None):
     s = requests.Session()
@@ -35,23 +33,19 @@ def init_session(token=None):
     s.verify = 'bundle-ca' if os.path.exists("bundle-ca") else True
     return s
 
-
 def parse_links(file_path):
     with open(file_path, 'r', encoding='utf-8') as f:
         return [line.strip() for line in f if line.strip()]
 
-
-# ---------------------- Gitee ----------------------
-def process_gitee(url, config):
-    base_url = config.get("gitee", "gitee-url")
-    token = config.get("gitee", "token")
+# ---------------------- Gitee / GitCode ----------------------
+def process_gitee_or_gitcode(url, config, platform):
+    base_url = config.get(platform, f"{platform}-url")
+    token = config.get(platform, "token")
     session = init_session(token)
-    m = re.match(r"https://gitee.com/([^/]+)/([^/]+)/pulls/(\d+)", url)
-    if not m:
-        return None
 
-    owner, repo, pr_id = m.groups()
-
+    m_pr = re.match(r"https://(gitee\.com|gitcode\.net)/([^/]+)/([^/]+)/pulls/(\d+)", url)
+    if m_pr:
+        _, owner, repo, pr_id = m_pr.groups()
     api_url = f"{base_url}/api/v5/repos/{owner}/{repo}/pulls/{pr_id}"
     files_url = f"{api_url}/files"
     pr = session.get(api_url).json()
@@ -67,6 +61,21 @@ def process_gitee(url, config):
         pr['base']['ref'], f"{owner}/{repo}", additions, deletions, reviewers
     ]
 
+    m_commit = re.match(r"https://(gitee\.com|gitcode\.net)/([^/]+)/([^/]+)/commit/([0-9a-fA-F]+)", url)
+    if m_commit:
+        _, owner, repo, sha = m_commit.groups()
+        commit_url = f"{base_url}/api/v5/repos/{owner}/{repo}/commits/{sha}"
+        commit = session.get(commit_url).json()
+
+        additions = commit.get('stats', {}).get('additions', 0)
+        deletions = commit.get('stats', {}).get('deletions', 0)
+        return [
+            commit['author']['name'], commit['author']['name'], commit['commit']['message'].splitlines()[0], url,
+            'committed', commit['commit']['author']['date'], '',
+            '', f"{owner}/{repo}", additions, deletions, ''
+        ]
+
+    return None
 
 # ---------------------- GitLab ----------------------
 def process_gitlab(url, config):
@@ -97,17 +106,25 @@ def process_gitlab(url, config):
 
 
 # ---------------------- CodeHub ----------------------
-def process_codehub(url, config):
-    # check merge requests
-    base_url = config.get("codehub", "codehub-url")
-    token = config.get("codehub", "token")
+def process_codehub_or_opencodehub(url, config, platform):
+    base_url = config.get(platform, f"{platform}-url")
+    token = config.get(platform, "token")
     session = init_session(token)
-    m = re.match(r"https://([^/]+)/([^/]+/[^/]+)/merge_requests/(\d+)", url.replace('#/', ''))
-    if m:
-        domain, repo_path, mr_id = m.groups()
+
+    if platform == "opencodehub":
+        mr_match = re.match(r"https://([^/]+)/OpenSourceCenter_CR/([^/]+/[^/]+)/-/change_requests/(\d+)", url.replace('#/', ''))
+        commit_match = re.match(r"https://([^/]+)/OpenSourceCenter_CR/([^/]+/[^/]+)/-/commit/([0-9A-Fa-f]+)", url.replace('#/', ''))
+        project_prefix = "OpenSourceCenter_CR%2F"
+    else:
+        mr_match = re.match(r"https://([^/]+)/([^/]+/[^/]+)/merge_requests/(\d+)", url.replace('#/', ''))
+        commit_match = re.match(r"https://([^/]+)/([^/]+/[^/]+)/files/commit/([0-9A-Fa-f]+)", url.replace('#/', ''))
+        project_prefix = ""
+
+    if mr_match:
+        domain, repo_path, mr_id = mr_match.groups()
         encoded_path = urllib.parse.quote(repo_path, safe='')
-        api_url = f"{base_url}/api/v4/projects/{encoded_path}/isource/merge_requests/{mr_id}"
-        changes_url = f"{base_url}/api/v4/projects/{encoded_path}/isource/merge_requests/{mr_id}/changes"
+        api_url = f"{base_url}/api/v4/projects/{project_prefix}{encoded_path}/isource/merge_requests/{mr_id}"
+        changes_url = f"{api_url}/changes"
         pr = session.get(api_url).json()
         changes = session.get(changes_url).json()
 
@@ -115,55 +132,28 @@ def process_codehub(url, config):
         deletions = sum(int(f['removed_lines']) for f in changes.get('changes', []))
         reviewers = pr.get('merge_request_reviewer_list', [])
         reviewer_names = ', '.join([r['name'] for r in reviewers]) if reviewers else ""
-    # check commits
-    m = re.match(r"https://([^/]+)/([^/]+/[^/]+)/files/commit/([0-9A-Fa-f]+)", url.replace('#/', ''))
-    if m:
-        domain, repo_path, commit_id = m.groups()
+
+        return [
+            pr['author']['name'], pr['author']['username'], pr['title'], url,
+            pr['state'], pr['created_at'], pr['merged_at'],
+            pr['target_branch'], repo_path, additions, deletions, reviewer_names
+        ]
+
+    if commit_match:
+        domain, repo_path, commit_id = commit_match.groups()
         encoded_path = urllib.parse.quote(repo_path, safe='')
-        api_url = f"{base_url}/api/v4/projects/{encoded_path}/repository/commits/{commit_id}"
+        api_url = f"{base_url}/api/v4/projects/{project_prefix}{encoded_path}/repository/commits/{commit_id}"
         commit = session.get(api_url).json()
+        additions = commit.get('stats', {}).get('additions', 0)
+        deletions = commit.get('stats', {}).get('deletions', 0)
 
-        additions = sum(int(f['additions']) for f in commit.get('changes', []))
-        deletions = sum(int(f['deletions']) for f in commit.get('changes', []))
-        #reviewers = pr.get('merge_request_reviewer_list', [])
-        #reviewer_names = ', '.join([r['name'] for r in reviewers]) if reviewers else ""
+        return [
+            commit['author_name'], commit['author_name'], commit['title'], url,
+            'committed', commit['created_at'], '', '', repo_path,
+            additions, deletions, ''
+        ]
 
-    if not m:
-        return None
-
-    return [
-        pr['author']['name'], pr['author']['username'], pr['title'], url,
-        pr['state'], pr['created_at'], pr['merged_at'],
-        pr['target_branch'], repo_path, additions, deletions, reviewer_names
-    ]
-
-
-# ---------------------- OpenCodeHub ----------------------
-def process_opencodehub(url, config):
-    m = re.match(r"https://([^/]+)/OpenSourceCenter_CR/([^/]+/[^/]+)/-/change_requests/(\d+)", url.replace('#/', ''))
-    if not m:
-        return None
-    domain, repo_path, mr_id = m.groups()
-    base_url = config.get("opencodehub", "opencodehub-url")
-    token = config.get("opencodehub", "token")
-    session = init_session(token)
-
-    encoded_path = urllib.parse.quote(repo_path, safe='')
-    api_url = f"{base_url}/api/v4/projects/OpenSourceCenter_CR%2F{encoded_path}/isource/merge_requests/{mr_id}"
-    changes_url = f"{base_url}/api/v4/projects/OpenSourceCenter_CR%2F{encoded_path}/isource/merge_requests/{mr_id}/changes"
-    pr = session.get(api_url).json()
-    changes = session.get(changes_url).json()
-
-    additions = sum(int(f['added_lines']) for f in changes.get('changes', []))
-    deletions = sum(int(f['removed_lines']) for f in changes.get('changes', []))
-    reviewers = pr.get('merge_request_reviewer_list', [])
-    reviewer_names = ', '.join([r['name'] for r in reviewers]) if reviewers else ""
-
-    return [
-        pr['author']['name'], pr['author']['username'], pr['title'], url,
-        pr['state'], pr['created_at'], pr['merged_at'],
-        pr['target_branch'], repo_path, additions, deletions, reviewer_names
-    ]
+    return None
 
 
 # ---------------------- Gerrit ----------------------
@@ -218,13 +208,15 @@ def main():
     for link in links:
         try:
             if 'gitee.com' in link:
-                row = process_gitee(link, config)
+                row = process_gitee_or_gitcode(link, config, 'gitee')
+            elif 'gitcode.net' in link:
+                row = process_gitee_or_gitcode(link, config, 'gitcode')
             elif 'gitlab' in link:
                 row = process_gitlab(link, config)
-            elif 'codehub-y' in link:
-                row = process_codehub(link, config)
+            elif 'codehub' in link:
+                row = process_codehub_or_opencodehub(link, config, 'codehub')
             elif 'open.codehub' in link:
-                row = process_opencodehub(link, config)
+                row = process_codehub_or_opencodehub(link, config, 'opencodehub')
             elif 'gerrit' in link or 'mgit' in link:
                 row = process_gerrit(link, config)
             else:
